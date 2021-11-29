4c68b48141035164f095c0ea374fb2e0879fe5c5
Daniel <supertuxcar@gmail.com>
2021-11-29 14:27:38 +0100

    fin1

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -14,21 +14,34 @@ public class PantallaHoraYFecha {
         horas = new PantallaDosDigitos(0,24);
         minutos = new PantallaDosDigitos(0,60);
     }
-    
+
     public String getFecha() {
         String fechaExacta = minutos.getTextoDeLaPantalla() + ":" + horas.getTextoDeLaPantalla() + dia.getTextoDeLaPantalla() + "-" + mes.getTextoDeLaPantalla() + "-" + ano.getTextoDeLaPantalla();
         return fechaExacta;
     }
-    
-     public void avanzarMinuto() {
+
+    public void avanzarMinuto() {   
         minutos.incrementaValorAlmacenado();
+        if (minutos.getValorAlmacenado() == 0){
+            horas.incrementaValorAlmacenado();
+        }
+        if (horas.getValorAlmacenado() == 0){
+            dia.incrementaValorAlmacenado();
+        }
+        if (dia.getValorAlmacenado() == 0){
+            mes.incrementaValorAlmacenado();
+        }
+        if (mes.getValorAlmacenado() == 0){
+            ano.incrementaValorAlmacenado();
+        }
+
     }
-    
-     public void setHora(PantallaDosDigitos nuevoMinuto, PantallaDosDigitos nuevoHora, PantallaDosDigitos nuevoDia, PantallaDosDigitos nuevoMes, PantallaDosDigitos nuevoAno) {
-        minutos = nuevoMinuto;
-        horas = nuevoHora;
-        dia = nuevoDia;
-        mes = nuevoMes;
-        ano = nuevoAno;
+
+    public void setHora(int numero1, int numero2, int numero3, int numero4, int numero5) {
+        minutos.setValorAlmacenado(numero1);
+        horas.setValorAlmacenado(numero2);
+        dia.setValorAlmacenado(numero3);
+        mes.setValorAlmacenado(numero4);
+        ano.setValorAlmacenado(numero5);
     }
 }  
