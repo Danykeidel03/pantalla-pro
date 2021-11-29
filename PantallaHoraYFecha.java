91d761a3b0d77a0d3a99dd46e50340666f6e35ed
Daniel <supertuxcar@gmail.com>
2021-11-29 13:12:29 +0100

    todoa4

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -6,9 +6,14 @@ public class PantallaHoraYFecha {
     private PantallaDosDigitos horas;
     private PantallaDosDigitos minutos;
 
-    public PantallaHoraYFecha () {
+    public PantallaHoraYFecha() 
+    {
+        dia = new PantallaDosDigitos(0,30);
+        mes = new PantallaDosDigitos(0,12);
+        ano = new PantallaDosDigitos(0,99);
+        horas = new PantallaDosDigitos(0,24);
+        minutos = new PantallaDosDigitos(0,60);
     }
-
 
 
 }  
