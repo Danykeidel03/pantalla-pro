64dbf43c437300fdd376aec9c277d35c81c1f505
Daniel <supertuxcar@gmail.com>
2021-11-29 13:52:19 +0100

    fin

--- PantallaHoraYFecha.java
+++ PantallaHoraYFecha.java
@@ -14,6 +14,21 @@ public class PantallaHoraYFecha {
         horas = new PantallaDosDigitos(0,24);
         minutos = new PantallaDosDigitos(0,60);
     }
-
-
+    
+    public String getFecha() {
+        String fechaExacta = minutos.getTextoDeLaPantalla() + ":" + horas.getTextoDeLaPantalla() + dia.getTextoDeLaPantalla() + "-" + mes.getTextoDeLaPantalla() + "-" + ano.getTextoDeLaPantalla();
+        return fechaExacta;
+    }
+    
+     public void avanzarMinuto() {
+        minutos.incrementaValorAlmacenado();
+    }
+    
+     public void setHora(PantallaDosDigitos nuevoMinuto, PantallaDosDigitos nuevoHora, PantallaDosDigitos nuevoDia, PantallaDosDigitos nuevoMes, PantallaDosDigitos nuevoAno) {
+        minutos = nuevoMinuto;
+        horas = nuevoHora;
+        dia = nuevoDia;
+        mes = nuevoMes;
+        ano = nuevoAno;
+    }
 }  
